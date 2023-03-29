ac1b9638d4494017446392e3e32643c91b3d9254
Jakub Stadnik <106155603+jakubstadnik03@users.noreply.github.com>
2023-03-29 08:27:44 +0200

    c

---
+++ pages/poukaz.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+function Poukaz() {
+  return (
+    <>
+      <Head>
+        <title>Dárkový poukaz | Lenka Design</title>
+      </Head>
+      <div className="pozvanka-block">
+        <p>Pokud máte zájem, je možné si zakoupit dárkový poukaz.</p>
+      </div>
+    </>
+  );
+}
+
+export default Poukaz;
